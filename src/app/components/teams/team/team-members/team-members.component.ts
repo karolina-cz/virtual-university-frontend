1e5a8abb5f2244b82bd950b3080edcee54860ba5
karolina <karolina.czachorska@gmail.com>
2021-01-06 16:30:20 +0100

    add template removing and adding team members

--- src/app/components/teams/team/team-members/team-members.component.ts
+++ src/app/components/teams/team/team-members/team-members.component.ts
@@ -27,6 +27,9 @@ export class TeamMembersComponent implements OnInit {
     new User('abcdef', 'Anna', 'Nowak', true),
     new User('mksxnak', 'Piotr', 'Kowalski', true)];
   autoCompleteItems: Observable<User[]>;
+  removedMembers: User[] = [];
+  addedMembers: User[] = [];
+  membersCopy: User[] = [];
   @Input() members: User[];
   constructor(private teamsService: TeamsService) { }
 
@@ -38,22 +41,56 @@ export class TeamMembersComponent implements OnInit {
   }
   onCancelClicked(){
     this.isEditingModeOn = !this.isEditingModeOn;
+    this.members = this.membersCopy;
+    this.resetEditValues();
   }
 
   onSaveClicked(){
     this.isEditingModeOn = !this.isEditingModeOn;
+    for (let i = 0; i < this.removedMembers.length; i++){
+      for (let j = 0; j < this.addedMembers.length; j++){
+        if (this.removedMembers[i].username === this.addedMembers[j].username){
+          this.addedMembers.splice(j, 1);
+          this.removedMembers.splice(i, 1);
+        }
+      }
+    }
+    console.log(this.removedMembers);
+    console.log(this.addedMembers);
+    this.resetEditValues();
+  }
+
+  resetEditValues(){
+    this.membersCopy = [];
+    this.addedMembers = [];
+    this.removedMembers = [];
+  }
+
+  onRemoveMemberClicked(member, index){
+    if (! (this.removedMembers.filter(e => e.username === member.username).length > 0)){
+      this.removedMembers.push(member);
+    }
+    this.members.splice(index, 1);
   }
 
   private _filter(value: string){
     const items = this.teamsService.getUsersByPattern();
     return items;
-    // const filterValue = value.toLowerCase();
-    //
-    // return this.allItems.filter(option => option.firstname.toLowerCase().indexOf(filterValue) === 0);
   }
 
-  optionClicked(option: User){
-    this.members.push(option);
+  optionClicked(member: User){
+    console.log(this.addedMembers.includes(member));
+    if (! (this.addedMembers.filter(e => e.username === member.username).length > 0)){
+      this.addedMembers.push(member);
+    }
+    if (! (this.members.filter(e => e.username === member.username).length > 0)){
+      this.members.push(member);
+    }
     this.myControl.setValue('');
+  }
+
+  onEditClicked(){
+    this.isEditingModeOn = !this.isEditingModeOn;
+    this.members.forEach(val => this.membersCopy.push(Object.assign({}, val)));
   }
 }
